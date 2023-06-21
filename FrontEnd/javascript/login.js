584bb3d6982ea08108756e4f995f8d2bd0060963
Darknessson69008 <131368078+Darknessson69008@users.noreply.github.com>
2023-06-21 21:20:50 +0200

    modified:   Backend/package.json 	modified:   Backend/swagger.yaml 	modified:   FrontEnd/assets/style.css 	modified:   FrontEnd/index.html 	new file:   FrontEnd/javascript/login.js 	modified:   FrontEnd/login.html

---
+++ FrontEnd/javascript/login.js
@@ -0,0 +1,38 @@
+function connect(event){
+    event.preventDefault(); 
+    const logInput = {
+        email : document.querySelector('#email').value,
+        password : document.querySelector('#pass').value
+    };
+    console.log(logInput);
+    const storeId = JSON.stringify(logInput);
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: storeId
+    })
+    .then (resp => resp.json())
+    .then ((response) => {
+        if (!response.token) { 
+            alert("Erreur"); //eviter alerte, faire un display block sur un element none dans html
+            return;
+        }
+            
+        const token = JSON.stringify(response.token);
+        window.localStorage.setItem("token", token);
+        window.location = "./index.html"; 
+    })
+}; 
+
+
+const form = document.querySelector(".logForm")
+form.addEventListener("submit",(event)=> connect(event)) 
+
+// item dans script.js pour verif SI token
+// 
+// interet de mettre modifier partout? - celui de projets ok, autre sans effets
+// 
+// pas de message de confirmation pour la suppression
+// 
+// login => logout avec delete item
+// 
