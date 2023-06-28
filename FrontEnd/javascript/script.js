30e644c6d5af9b292752eb0a215a6e7c0d8e6b63
Darknessson69008 <131368078+Darknessson69008@users.noreply.github.com>
2023-06-28 11:13:21 +0200

    modified:   FrontEnd/assets/style.css 	modified:   FrontEnd/index.html 	modified:   FrontEnd/javascript/gallery.js 	new file:   FrontEnd/javascript/modal.js 	modified:   FrontEnd/javascript/script.js

--- FrontEnd/javascript/script.js
+++ FrontEnd/javascript/script.js
@@ -12,7 +12,6 @@ function showEdit(action){
 function logInorOut() {
     const tokenValue = window.localStorage.getItem('token');
     const loginout = document.querySelector("#loginout");
-    console.log(loginout)
     if (tokenValue != null){
         showEdit('show');
         loginout.innerText = "logout";
@@ -38,9 +37,71 @@ loginout.addEventListener("click", function(event){
 // dans modal, 1 bouton ajout et suppression
 // alerte pour confirmer "confirm()"
 
+//ouverture et fermeture modal
 
-const modal = document.querySelector('.modal')
-modal.addEventListener("click", function(event){
-    event.preventDefault();
-    window.location = "./index.html";
+let modal = null
+
+const openModal = function(e) {
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute('href'))
+    target.style.display = null
+    target.removeAttribute('aria-hidden')
+    target.setAttribute('aria-modal', 'true')
+    modal = target
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.modal_close').addEventListener('click', closeModal)
+    modal.querySelector('.modal_content').addEventListener('click', stopPropagation)
+}
+
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault()
+    modal.style.display = "none"
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.modal_close').removeEventListener('click', closeModal)
+    modal.querySelector('.modal_content').removeEventListener('click', stopPropagation)
+    modal = null
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+document.querySelectorAll('.js_modal').forEach(a => {
+    a.addEventListener('click', openModal)    
 })
+
+
+/// fonction suppression de type :
+
+function deleteImage(id, token){
+    const url = `http://localhost:5678/api/works/${id}`;
+    const request = {
+        method: "DELETE",
+        headers: {
+            "Authorization": `Bearer ${token}`
+            }
+        };
+    const response = fetch( url, request);
+}
+
+/// pour resultat sur affichage modal, rappeler l'api de get
+
+/// fonction ajout de type :
+
+function addImage(bodyData, token) {
+    const url = "http://localhost:5678/api/works";      
+    const request = {
+        method: "POST",
+        headers: {
+         Authorization: `Bearer ${token}`
+        },
+        body: bodyData
+    };
+    const response = fetch( url, request);
+}
+
+
+//une modal 1 none 1 block au click
